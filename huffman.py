e790d34aebdd650497735cfc526bf5975855577e
kajetan <kajetanzdanowicz@wp.pl>
2021-04-14 19:18:12 +0200

    pq used to build tree

--- huffman.py
+++ huffman.py
@@ -1,4 +1,6 @@
 import os
+from heapq import heappop, heappush, heapify
+
 
 INPUT_FILE = "input.txt"
 OUTPUT_FILE = "output.txt"
@@ -17,8 +19,43 @@ class Node:
         return self.frequency < second.frequency
 
     def __str__(self):
-        return "character: {}  frequency: {}".format(self.character,
+        return "character: {}  frequency: {} \n".format(self.character,
                                                                 self.frequency)
+
+
+class Tree:
+
+    def __init__(self, text):
+        if len(text) == 0:
+            return
+        
+        frequency = {i : text.count(i) for i in set(text)}
+        self.priority_queue = [Node(key, value) for key, value in frequency.items()]
+        heapify(self.priority_queue)
+        print(self)
+
+        while len(self.priority_queue) != 1:
+            r_child = heappop(self.priority_queue)
+            l_child = heappop(self.priority_queue)
+            frequency_sum = r_child.frequency + l_child.frequency
+            new_node = Node(None, frequency_sum, left_child=l_child, right_child=r_child)
+            heappush(self.priority_queue, new_node)
+
+
+    def encode():
+        pass
+
+
+    def decode():
+        pass
+
+
+    def __str__(self):
+        nodes_text = ""
+        for node in self.priority_queue:
+            nodes_text += node.__str__()
+        return nodes_text
+
 
 def list_to_str(array):
     text = ""
@@ -41,12 +78,11 @@ if __name__ == '__main__':
         input_text = input_file.readlines()
 
 ###################
-    node = Node('a', 32)
-    print(node)
-
+    input_text = "huffman xxxxxxxxxxx"
     mystring = list_to_str(input_text)
-    print(mystring)
+    print("Input: {}".format(mystring))
 
+    tree = Tree(mystring)
 
 ####################
     if not os.path.isfile('./{}'.format(OUTPUT_FILE)):
