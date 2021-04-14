13a0f23b61a98e870206fbb1320f3d1dd8294c86
kajetan <kajetanzdanowicz@wp.pl>
2021-04-14 17:14:13 +0200

    list to str, exceptions

--- huffman.py
+++ huffman.py
@@ -1,16 +1,28 @@
 import os
 
+
+def list_to_str(array):
+    text = ""
+    for element in array:
+        text += element
+    return text
+
+
 if __name__ == '__main__':
 
     if not os.path.isfile('./input.txt'):
         os.mknod("input.txt")
         raise Exception("Type text to be encoded to input.txt")
 
+    if not os.path.getsize("./input.txt"):
+        raise Exception("input.txt is empty")
+
     input_text = []
     with open('./input.txt') as input_file:
         input_text = input_file.readlines()
 
-    print([line for line in input_text])
+    mystring = list_to_str(input_text)
+    print(mystring)
 
     if not os.path.isfile('./output.txt'):
         os.mknod("output.txt")
