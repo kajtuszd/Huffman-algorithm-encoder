4fab4a13d88806cdb25106eceda2869fef71dcf1
kajetan <kajetanzdanowicz@wp.pl>
2021-04-14 23:56:15 +0200

    decoding works

--- huffman.py
+++ huffman.py
@@ -13,7 +13,7 @@ class Node:
         self.character = character
         self.frequency = frequency
         self.left_child = left_child
-        self.right_child= right_child
+        self.right_child = right_child
 
     def __lt__(self, second):
         return self.frequency < second.frequency
@@ -26,17 +26,18 @@ class Node:
         return self.left_child is None and self.right_child is None
 
 
+
 class Tree:
 
-    def __init__(self, text):
-        if len(text) == 0:
-            return
-        
-        frequency = {i : text.count(i) for i in set(text)}
+    def __init__(self, data):
+        self.data = data
+        frequency = {i : data.count(i) for i in list(self.data)}
         self.priority_queue = [Node(key, value) for key, value in frequency.items()]
-        heapify(self.priority_queue)
-        print(self)
+        # print(self) # uncomment to see each character frequency
 
+    
+    def compute(self):
+        heapify(self.priority_queue)
         while len(self.priority_queue) != 1:
             r_child = heappop(self.priority_queue)
             l_child = heappop(self.priority_queue)
@@ -46,24 +47,50 @@ class Tree:
 
         codes = {}
         Tree.encode(self.priority_queue[0], "", codes)
+        # print("Codes: {}\n".format(codes)) # uncomment to see each letter in binary
+        encoded_text = ""
 
-        print("Codes: ", codes)
+        for character in self.data:
+            encoded_text += codes.get(character)
+
+        # print("Encoded text: {} \n".format(encoded_text)) # uncomment to see encoded text
+        print("Decoded text: ", end="")
+
+        if self.priority_queue[0].has_no_children():
+            while self.priority_queue[0].frequency > 0:
+                print(self.priority_queue[0].character, end='')
+                self.priority_queue[0].frequency -= 1
+        else:
+            index = -1
+            while index < len(encoded_text) - 1:
+                index = Tree.decode(self.priority_queue[0], index, encoded_text)
+        return encoded_text
 
 
     @staticmethod
-    def encode(root, encoded_text, code):
-        if root is None:
+    def encode(top, encoded_text, code):
+        if top is None:
             return
 
-        if root.has_no_children():
-            code[root.character] = encoded_text if len(encoded_text) > 0 else '1'
+        if top.has_no_children():
+            code[top.character] = encoded_text if len(encoded_text) > 0 else '1'
 
-        Tree.encode(root.left_child, encoded_text + '0', code)
-        Tree.encode(root.right_child, encoded_text + '1', code)
+        Tree.encode(top.left_child, encoded_text + '0', code)
+        Tree.encode(top.right_child, encoded_text + '1', code)
 
 
-    def decode():
-        pass
+    @staticmethod
+    def decode(top, pointer, encoded_text):
+        if top is None:
+            return pointer
+
+        if top.has_no_children():
+            print(top.character, end='')
+            return pointer
+        
+        pointer += 1
+        top = top.left_child if encoded_text[pointer] == '0' else top.right_child
+        return Tree.decode(top, pointer, encoded_text)
 
 
     def __str__(self):
@@ -71,6 +98,7 @@ class Tree:
         for node in self.priority_queue:
             nodes_text += node.__str__()
         return nodes_text
+
 
 
 def list_to_str(array):
@@ -93,18 +121,16 @@ if __name__ == '__main__':
     with open('./{}'.format(INPUT_FILE)) as input_file:
         input_text = input_file.readlines()
 
-###################
-    input_text = "huffman xxxxxxxxxxx"
-    mystring = list_to_str(input_text)
-    print("Input: {}".format(mystring))
+    text = list_to_str(input_text)
+    # print("Input: {} \n".format(text)) # uncomment to see input
 
-    tree = Tree(mystring)
+    tree = Tree(text)
+    encoded_text = tree.compute()
 
-####################
     if not os.path.isfile('./{}'.format(OUTPUT_FILE)):
         os.mknod("{}".format(OUTPUT_FILE))
 
     output_file = open("./{}".format(OUTPUT_FILE), 'w')
-    output_file.write("hello world")
+    output_file.write(encoded_text)
     output_file.close()
     print("\n")
