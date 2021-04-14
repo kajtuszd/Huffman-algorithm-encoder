09024a848c8c5bef2ff018ed29366421f7b78c19
kajetan <kajetanzdanowicz@wp.pl>
2021-04-14 18:05:18 +0200

    node to store data

--- huffman.py
+++ huffman.py
@@ -3,6 +3,23 @@ import os
 INPUT_FILE = "input.txt"
 OUTPUT_FILE = "output.txt"
 
+
+class Node:
+    
+    def __init__(self, character, frequency, left_child=None, 
+                                                            right_child=None):
+        self.character = character
+        self.frequency = frequency
+        self.left_child = left_child
+        self.right_child= right_child
+
+    def __lt__(self, second):
+        return self.frequency < second.frequency
+
+    def __str__(self):
+        return "character: {}  frequency: {}".format(self.character,
+                                                                self.frequency)
+
 def list_to_str(array):
     text = ""
     for element in array:
@@ -23,9 +40,15 @@ if __name__ == '__main__':
     with open('./{}'.format(INPUT_FILE)) as input_file:
         input_text = input_file.readlines()
 
+###################
+    node = Node('a', 32)
+    print(node)
+
     mystring = list_to_str(input_text)
     print(mystring)
 
+
+####################
     if not os.path.isfile('./{}'.format(OUTPUT_FILE)):
         os.mknod("{}".format(OUTPUT_FILE))
 
